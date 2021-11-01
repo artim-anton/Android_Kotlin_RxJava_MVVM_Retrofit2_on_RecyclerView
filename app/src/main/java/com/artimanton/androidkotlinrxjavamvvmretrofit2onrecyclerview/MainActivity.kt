649fe66f2c880b719ddee93339d90fa45c7b5448
artim-anton <1rimola0>
2021-11-01 11:46:29 +0200

    Initial commit

---
+++ app/src/main/java/com/artimanton/androidkotlinrxjavamvvmretrofit2onrecyclerview/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.artimanton.androidkotlinrxjavamvvmretrofit2onrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
+import com.artimanton.androidkotlinrxjavamvvmretrofit2onrecyclerview.adapter.BookListAdapter
+import com.artimanton.androidkotlinrxjavamvvmretrofit2onrecyclerview.nework.BookListModel
+import com.artimanton.androidkotlinrxjavamvvmretrofit2onrecyclerview.viewmodel.MainActivityViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel:  MainActivityViewModel
+    lateinit var bookListAdapter: BookListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initSearchBox()
+        initRecyclerView()
+    }
+
+    fun initSearchBox() {
+        inputBookName.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                loadAPIData(s.toString())
+            }
+        })
+    }
+
+    fun initRecyclerView(){
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val decoration  = DividerItemDecoration(applicationContext, VERTICAL)
+            addItemDecoration(decoration)
+            bookListAdapter = BookListAdapter()
+            adapter =bookListAdapter
+        }
+    }
+
+    fun loadAPIData(input: String) {
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.getBookListObserver().observe(this, Observer<BookListModel>{
+            if(it != null) {
+                bookListAdapter.bookListData = it.items
+                bookListAdapter.notifyDataSetChanged()
+            }
+            else {
+                //Toast.makeText(this, "Error in fetching data", Toast.LENGTH_SHORT).show()
+            }
+        })
+        viewModel.makeApiCall(input)
+    }
+}
